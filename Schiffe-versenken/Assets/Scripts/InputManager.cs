62e33642aade7f178db9c8e4ca814164907658db
Leon <Leonisos@gmx.de>
2025-07-16 21:05:37 +0200

    Unity Project neu aufgesetzt

---
+++ Schiffe-versenken/Assets/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera sceneCamera;
+    private Vector3 lastPosition;
+
+    [SerializeField]
+    private LayerMask placementLayermask;
+
+    //Create a Event listiner
+    public event Action OnClicked, OnExit;
+
+    private void Update() {
+        //check if Event is triggered
+        if (Input.GetMouseButtonDown(0))
+            OnClicked.Invoke();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnExit.Invoke();
+    }
+
+    /**
+    *   Check if mouse is over UI
+    *@return    Returns true if EventSystem sayes that mouse is over UI
+    **/
+    public bool IsPointerOverUI()
+        => EventSystem.current.IsPointerOverGameObject();
+
+    public Vector3 GetSelectedMapPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = sceneCamera.nearClipPlane;
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, placementLayermask))
+        {
+            lastPosition = hit.point;
+        }
+
+        return lastPosition;
+    }
+
+}
